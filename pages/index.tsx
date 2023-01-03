b6ceb411965dd29754ba6bf725655c051caadcb5
Robert Shepley <shepleysound@gmail.com>
2023-01-03 14:37:58 -0800

    Correct Tailwind CSS branding.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>Next.js + Tailwind CSS</title>
         <meta
           name='description'
-          content='A starter project for Next.js and TailwindCSS'
+          content='A starter project for Next.js and Tailwind CSS'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
